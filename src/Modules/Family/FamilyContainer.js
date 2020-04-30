69ea6823a262148ba423fc0ef155611a450e7b8f
akshay.s <akshay.s@inapp.com>
2020-04-30 14:46:29 +0530

    updated registration form , event details added as different component

--- src/Modules/Family/FamilyContainer.js
+++ src/Modules/Family/FamilyContainer.js
@@ -8,6 +8,7 @@ import RegistrationTextComponent from './RegistrationTextComponent';
 import {confirm, showMessage} from '../../Utils/Util';
 import FooterContainer from "../Footer/FooterContainer";
 import HeaderComponent from "../Header/HeaderComponent";
+import EventDescriptionFormComponent from "../Events/EventDescriptionFormComponent";
 import '../../Assets/scss/main.scss';
 import back from '../../Assets/img/back.svg';
 import useForm from '../../Utils/UseForm';
@@ -76,8 +77,6 @@ const FamilyContainer = () => {
         };
 
         if(familyDetails) {
-
-
         } else {
             showMessage('error', 'Something went wrong');
         }
@@ -121,26 +120,7 @@ const FamilyContainer = () => {
                         </div>
                         <div className="row">
                             <div className="col-lg-8 col-md-6">
-                                <div className="content-wrapper">
-                                    <div className="medium-title font-weight-bold">
-                                        Kroger Food Pantry EXPRESS
-                                    </div>
-                                    <div className="d-flex">
-                                        <div className="pantry-name flex-grow-1">Prepack Pantry</div>
-                                      
-                                    </div>
-                                    <div className="d-flex flex-column mt-3 mb-3 time-wrapper">
-                                        <span>Selected Time</span>
-                                        <span className="font-weight-bold time-value">1:00 pm</span>
-                                        <span>Monday, 4/2/2020</span>
-                                    </div>
-                                    <div className="d-flex flex-column mt-3 mb-3 address-wrapper">
-                                        <span>225 E Gates St,</span>
-                                        <span>Columbus, OH 43206</span>
-                                        <span>(321) 456-0987</span>
-                                    </div>
-                                </div>
-
+                                <EventDescriptionFormComponent/>
                             </div>
                             <div className="col-lg-4 col-md-6">
                                 <RegistrationTextComponent/>
