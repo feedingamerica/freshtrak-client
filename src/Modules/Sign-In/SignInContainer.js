c5f3e9a44b9ef11b738e807c95fc6baf197b2b19
Sean Lawrenz <seanlawrenz@megans-mbp.lan>
2020-05-03 12:22:39 -0400

    Basic form added

--- src/Modules/Sign-In/SignInContainer.js
+++ src/Modules/Sign-In/SignInContainer.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import SignInFormComponent from './SignInFormComponent';
 
-const SignInContainer = () => (<div><h1>YO</h1></div>)
+const SignInContainer = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => { console.log(data) };
+
+  return (
+    <div className="d-flex justify-content-center">
+      <form onSubmit={handleSubmit(onSubmit)} className="sign-in-form">
+        <SignInFormComponent register={register} errors={errors} />
+        <div className="button-wrap mt-4">
+          <button
+            type="submit"
+            className="btn custom-button"
+            data-testid="log in button"
+          >Log In</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
 
 export default SignInContainer;
