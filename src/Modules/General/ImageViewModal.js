1f2ef879a1c761ece5c5ab0546a51ff9221b0005
Ashik Varma <varmaashik@gmail.com>
2020-05-11 11:02:39 +0530

    New ImageModal styles added

--- src/Modules/General/ImageViewModal.js
+++ src/Modules/General/ImageViewModal.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import './image_modal_style.css'
+// import './image_modal_style.css'
 const ImageViewModal = (props)=>{
     return (
         <React.Fragment>
             <div id="myModal" className="image-modal">
-                <span className="close close-show" id="imageView" style={{}} onClick={props.close} >&times;</span>
-                <img className="image-modal-content" id="img01" src={props.src} />
+               <div className="image-modal-wrap">
+               <span className="close close-show" id="imageView" style={{}} onClick={props.close} >&times;</span>
+               <div className="image-viewser">
+               <img className="image-modal-content" id="img01" src={props.src} />
+               </div>
+               </div>
             </div>
         </React.Fragment>
     );
