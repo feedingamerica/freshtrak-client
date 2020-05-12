a00593339b05c0d51b8486f0e6e845c80634c0b4
Basiltj07 <basiltj07@gmail.com>
2020-05-12 11:18:11 +0530

    HouseHold form update

--- src/Modules/Family/__test__/houseHoldForm.test.js
+++ src/Modules/Family/__test__/houseHoldForm.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render,fireEvent, wait} from '@testing-library/react';
 import HouseHoldFormComponent from '../HouseHoldFormComponent';
-import { noop, mockHouseHoldBuilder } from '../../../Testing';
+import { noop, mockHouseHold } from '../../../Testing';
 import {fake,oneOf} from 'test-data-bot'
 
 
@@ -16,11 +16,11 @@ test('should render HouseHoldFormComponent with no data', () => {
   }).not.toThrowError();
 });
 
-test('should render HouseHoldFormComponent with mockHouseHoldBuilder', () => {
+test('should render HouseHoldFormComponent with Mock data', () => {
   expect(() => {
     render(
     <HouseHoldFormComponent
-    onSelectedChild={noop}
+    onSelectedChild={mockHouseHold}
     onFormErrors={noop}
     />
   );
@@ -29,6 +29,7 @@ test('should render HouseHoldFormComponent with mockHouseHoldBuilder', () => {
 
 
 test('should have proper binding onChange',()=>{
+  let mockData = mockHouseHold();
     const {container,getByTestId,queryByTestId,} = render(
         <HouseHoldFormComponent
             onSelectedChild={noop}
@@ -36,30 +37,47 @@ test('should have proper binding onChange',()=>{
         />);
 
 
-  const hType = container.querySelector('select[name="housing_type"]');
-  const zipValue = container.querySelector('input[name="zip_code"]');
-  const aptValue = container.querySelector('input[name="apt_no"]');
-  const streetValue = container.querySelector('input[name="street_address"]');
+  /*const hType = container.querySelector('select[name="housing_type"]');*/
+  /*const zipValue = container.querySelector('input[name="zip_code"]');*/
+  /*const aptValue = container.querySelector('input[name="apt_no"]');*/
+  /*const streetValue = container.querySelector('input[name="street_address"]');*/
 
-  const fakeHousingType = oneOf('Apartment','Mobile home or house trailer', 'Military housing','Student housing','Temporary','Prefer not to answer').generate(1);
+  /*const fakeHousingType = oneOf('Apartment','Mobile home or house trailer', 'Military housing','Student housing','Temporary','Prefer not to answer').generate(1);*/
 
-  const fakStreetAddress= fake((f) => f.address.streetAddress()).generate(1);
+  /*const fakStreetAddress= fake((f) => f.address.streetAddress()).generate(1);
   const fakeApt='Bakers A2 '
   const fakeZip= '686503'
   const fakeNumbers='123'
-  const fakeStringValue ='abc'
+  const fakeStringValue ='abc';*/
 
+  const housing_type = container.querySelector('select[name="housing_type"]');
+  const street_address = container.querySelector('input[name="street_address"]');
+  const apt_no = container.querySelector('input[name="apt_no"]');
+  const zip_code = container.querySelector('input[name="zip_code"]');
 
+  fireEvent.change(housing_type,{target:{value:mockData.housingType}});
+  expect(housing_type.value).toBe(mockData.housingType);
+
+  fireEvent.change(street_address,{target:{value:mockData.streetAddress}});
+  expect(street_address.value).toBe(mockData.streetAddress);
+
+  fireEvent.change(apt_no,{target:{value:mockData.aptNo}});
+  expect(apt_no).toHaveValue(mockData.aptNo);
+
+  fireEvent.change(zip_code,{target:{value:mockData.zip}});
+  expect(zip_code.value).toBe(mockData.zip);
+
+  /*console.log(mockData.zip);*/
 
      // for checking binding
 
-  fireEvent.change(hType,{target:{value:fakeHousingType}});
-  expect(hType.value).toBe(fakeHousingType);
+  /*fireEvent.change(hType,{target:{value:fakeHousingType}});
+  expect(hType.value).toBe(fakeHousingType);*/
 
-  fireEvent.change(zipValue,{target:{value:''}});
-  expect(zipValue.value).toBe('');
+  /*fireEvent.change(zipValue,{target:{value:''}});
+  expect(zipValue.value).toBe('');*/
 
-  fireEvent.change(zipValue,{target:{value:fakeZip}});
+ /* fireEvent.change(zipValue,{target:{value:fakeZip}});
   expect(zipValue.value).toBe(fakeZip);
 
 
@@ -80,11 +98,11 @@ test('should have proper binding onChange',()=>{
   expect(streetValue.value).toBe('');
 
   fireEvent.change(streetValue,{target:{value:fakeNumbers}});
-  expect(streetValue.value).toBe(fakeNumbers);
+  expect(streetValue.value).toBe(fakeNumbers);*/
 
 
-  fireEvent.change(streetValue,{target:{value:fakStreetAddress}});
-  expect(streetValue.value).toBe(fakStreetAddress);
+ /* fireEvent.change(streetValue,{target:{value:fakStreetAddress}});
+  expect(streetValue.value).toBe(fakStreetAddress);*/
 
 });
 
--- src/Testing/mock-family/index.js
+++ src/Testing/mock-family/index.js
@@ -1,42 +1,40 @@
 import { build, fake,oneOf } from 'test-data-bot';
 
 export const mockHouseHold = build('HouseHold').fields({
-  streetAddress: fake(f => f.address.streetAddress()),
-  aptNo: fake(f => f.random.number()),
-  zip: fake(f => f.address.zipCode()),
-  housingType:oneOf('Apartment','Mobile home or house trailer', 'Military housing','Student housing','Temporary','Prefer not to answer')
+    streetAddress: fake(f => f.address.streetAddress()),
+    aptNo: String(fake(f => f.random.number())),
+    zip: fake(f => f.address.zipCode()),
+    housingType:oneOf('Apartment','Mobile home or house trailer', 'Military housing','Student housing','Temporary','Prefer not to answer')
 });
+
 export const mockPassword = build('Password').fields({
-  password: fake(f => f.random.word())
+    password: fake(f => f.random.word())
 });
 
 export const mockPickUp = build('PickUp').fields({
-  pickupInfo: fake(f => f.random.word()),
-  pickupType:oneOf('Me','Some one Else'),
-  pickupName: fake(f => f.name.firstName()),
-  pickupNumberPlate: fake(f => f.random.number())
+    pickupInfo: fake(f => f.random.word()),
+    pickupType:oneOf('Me','Some one Else'),
+    pickupName: fake(f => f.name.firstName()),
+    pickupNumberPlate: fake(f => f.random.number())
 });
 
- const mockPrimary = build('Primary').fields({
-  firstName: fake(f => f.name.firstName()),
-  lastName: fake(f => f.name.lastName()),
-  middleName: fake(f => f.name.lastName()),
-  suffix: oneOf('Jr','Sr'),
-  dob: fake(f => f.date.past()),
-  hoh: oneOf('Yes','No'),
-  phoneNumber: fake(f => f.random.number()),
-  phoneNumberCheckBOx: oneOf('Yes','No'),
-  email: fake(f => f.internet.email()),
-  comPref:oneOf('Email','Phone')
+const mockPrimary = build('Primary').fields({
+    firstName: fake(f => f.name.firstName()),
+    lastName: fake(f => f.name.lastName()),
+    middleName: fake(f => f.name.lastName()),
+    suffix: oneOf('Jr','Sr'),
+    dob: fake(f => f.date.past()),
+    hoh: oneOf('Yes','No'),
+    phoneNumber: fake(f => f.random.number()),
+    phoneNumberCheckBOx: oneOf('Yes','No'),
+    email: fake(f => f.internet.email()),
+    comPref:oneOf('Email','Phone')
 });
-
 
 export const mockMemberCountBuilder = build('Household').fields({
-
     countSenior: fake(f => f.random.number()).generate(1),
     countMiddle: fake(f => f.random.number()).generate(1),
-    countJunior: fake(f => f.random.number()).generate(1),
-
+    countJunior: fake(f => f.random.number()).generate(1)
 });
 
 export const mockPrimaryInfoBuilder = mockPrimary();
