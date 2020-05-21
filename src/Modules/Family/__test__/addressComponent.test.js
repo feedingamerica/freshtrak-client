38823d62127c7dcde4ea7b981b69fdf48638ee00
Sean Lawrenz <seanlawrenz@megans-mbp.lan>
2020-05-20 22:14:12 -0400

    tests passing

--- src/Modules/Family/__test__/addressComponent.test.js
+++ src/Modules/Family/__test__/addressComponent.test.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import HouseHoldFormComponent from '../HouseHoldFormComponent';
+import AddressComponent from '../AddressComponent';
 import { noop } from '../../../Testing';
 
 test('should render', () => {
   expect(() => {
     render(
-      <HouseHoldFormComponent register={noop} errors={noop} />
+      <AddressComponent register={noop} errors={noop} />
     );
   }).not.toThrowError();
 });
 
 test('should show invalid form if required field is not filled out', () => {
   const { getByText } = render(
-    <HouseHoldFormComponent register={noop} errors={{ address_line_1: true }} />
+    <AddressComponent register={noop} errors={{ address_line_1: true }} />
   );
   getByText(/This field is required/i);
 });
