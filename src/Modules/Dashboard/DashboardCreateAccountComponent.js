6fb9b22f5aaeb3486bd3d6e3aa0ffcee071037d7
Sean Lawrenz <sean.lawrenz@mbp-77.lan>
2020-05-22 13:29:02 -0400

    removed out of scope buttons

--- src/Modules/Dashboard/DashboardCreateAccountComponent.js
+++ src/Modules/Dashboard/DashboardCreateAccountComponent.js
@@ -17,19 +17,20 @@ const DashboardCreateAccountComponent = () => {
       <h2 className="mb-5 font-weight-bold mobile-text-left text-center">
         FreshTrak is here to help!
       </h2>
-      <div className="row text-center">
+      <div className="row text-center justify-content-between">
         <BoxComponent
           title="Stay Up to Date"
           content="Make a FreshTrak account to stay up to date on local food access events."
           imageUrl={CalenderIcon}
           className="stay-up-to-date"
         />
-        <BoxComponent
+        {/* Out of scope */}
+        {/* <BoxComponent
           title="Pre-Register"
           content="Make a FreshTrak account to stay up to date on local food access events."
           imageUrl={PreRegisteredIcon}
           className="pre-register"
-        />
+        /> */}
         <BoxComponent
           title="Find Food"
           content="Enter your zip code and get connected to food access resources in your community."
@@ -37,7 +38,8 @@ const DashboardCreateAccountComponent = () => {
           className="find-food"
         />
       </div>
-      <div className="row mt-5 text-center">
+      {/* Out of scope */}
+      {/* <div className="row mt-5 text-center">
         <div className="col-12">
           <ButtonComponent
             type="button"
@@ -49,7 +51,7 @@ const DashboardCreateAccountComponent = () => {
             onClickfunction={handleClick}
           />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
