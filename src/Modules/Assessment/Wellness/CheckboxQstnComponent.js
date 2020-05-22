1d9e1fc66823767efeb547fc55562082fd3489c4
Rajesh KP <rajesh.kp@inapp.com>
2020-05-22 18:11:09 +0530

    checkbox in checkbox question ui fixes

--- src/Modules/Assessment/Wellness/CheckboxQstnComponent.js
+++ src/Modules/Assessment/Wellness/CheckboxQstnComponent.js
@@ -4,7 +4,7 @@ import ButtonComponent from '../../General/ButtonComponent'
 const CheckboxQstnComponent = (props) => {
 let {qstn,id} = props.content;
     return (
-		<>
+		<React.Fragment>
 
 							<div className="assesment-content flex-grow-1 flex-column justify-content-between d-flex mt-3 mb-3">
                                 <div className="a-c-content flex-grow-1">
@@ -12,69 +12,69 @@ let {qstn,id} = props.content;
                                     <p className="mt-2">You can choose more than one.</p>
                                     <div className="mt-4 checkbox-wrapper">
                                         <div className="form-group">
-                                            <label className="custom-checkbox">
-                                                <input type="checkbox" className="checkbox" />
-                                                <span>Working for pay part-time  (Less than 30 hours per week) </span>
-                                            </label>
+                                            <div className="checkbox-custom">
+                                                <input type="checkbox" id="part-time"/>
+                                                <label for="part-time">Working for pay part-time  (Less than 30 hours per week) </label>
+                                            </div>
                                         </div>
                                         <div className="form-group">
-                                            <label className="custom-checkbox">
-                                                <input type="checkbox" className="checkbox" />
-                                                <span>Working for pay full-time   (30 hours per week or more)</span>
-                                            </label>
+                                            <div className="checkbox-custom">
+                                                <input type="checkbox" id="pay-full-time"/>
+                                                <label for="pay-full-time">Working for pay full-time   (30 hours per week or more)</label>
+                                            </div>
                                         </div>
                                         <div className="form-group">
-                                            <label className="custom-checkbox">
-                                                <input type="checkbox" className="checkbox" />
-                                                <span>Pension</span>
-                                            </label>
+                                            <div className="checkbox-custom">
+                                                <input type="checkbox" id="pension"/>
+                                                <label for="pension">Pension</label>
+                                            </div>
                                         </div>
                                         <div className="form-group">
-                                            <label className="custom-checkbox">
-                                                <input type="checkbox" className="checkbox" />
-                                                <span>Social Security</span>
-                                            </label>
+                                            <div className="checkbox-custom">
+                                                <input type="checkbox" id="social-security"/>
+                                                <label for="social-security">Social Security</label>
+                                            </div>
                                         </div>
                                         <div className="form-group">
-                                            <label className="custom-checkbox">
-                                                <input type="checkbox" className="checkbox" />
-                                                <span>Unemployment compensation</span>
-                                            </label>
+                                            <div className="checkbox-custom">
+                                                <input type="checkbox" id="unemployment"/>
+                                                <label for="unemployment">Unemployment compensation</label>
+                                            </div>
                                         </div>
                                         <div className="form-group">
-                                            <label className="custom-checkbox">
-                                                <input type="checkbox" className="checkbox" />
-                                                <span>Disability (SSDI), workman’s compensation, or Supplemental Security Income (SSI)</span>
-                                            </label>
+                                            <div className="checkbox-custom">
+                                                <input type="checkbox" id="disability"/>
+                                                <label for="disability">Disability (SSDI), workman’s compensation, or Supplemental Security Income (SSI)</label>
+                                            </div>
                                         </div>
                                         <div className="form-group">
-                                            <label className="custom-checkbox">
-                                                <input type="checkbox" className="checkbox" />
-                                                <span>General assistance</span>
-                                            </label>
-                                        </div>
+                                            <div className="checkbox-custom">
+                                                    <input type="checkbox" id="general-assistance"/>
+                                                    <label for="general-assistance">General assistance</label>
+                                                </div>
+                                            </div>
                                         <div className="form-group">
-                                            <label className="custom-checkbox">
-                                                <input type="checkbox" className="checkbox" />
-                                                <span>Temporary Assistance to Needy Families also called TANF, formally AFDC</span>
-                                            </label>
-                                        </div>
+                                             <div className="checkbox-custom">
+                                                    <input type="checkbox" id="temporary-assistance"/>
+                                                    <label for="temporary-assistance">Temporary Assistance to Needy Families also called TANF, formally AFDC</label>
+                                                </div>
+                                            </div>
                                         <div className="form-group">
-                                            <label className="custom-checkbox">
-                                                <input type="checkbox" className="checkbox" />
-                                                <span>Government assistance with child care costs</span>
-                                            </label>
-                                        </div>
+                                            <div className="checkbox-custom">
+                                                    <input type="checkbox" id="government-assistance"/>
+                                                    <label for="government-assistance">Government assistance with child care costs</label>
+                                                </div>
+                                            </div>
                                         <div className="form-group">
-                                            <label className="custom-checkbox">
-                                                <input type="checkbox" className="checkbox" />
-                                                <span>Child support or alimony</span>
-                                            </label>
-                                        </div>
+                                            <div className="checkbox-custom">
+                                                    <input type="checkbox" id="child-support"/>
+                                                    <label for="child-support">Child support or alimony</label>
+                                                </div>
+                                            </div>
                                     </div>
                                 </div>
                             </div>
-		</>
+		</React.Fragment>
     )
 
 };
