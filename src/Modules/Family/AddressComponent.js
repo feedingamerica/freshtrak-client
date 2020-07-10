617d575a4054aecdda93c5882b305385aacc7030
David Pickering <davepickering1@gmail.com>
2020-07-10 09:48:24 -0400

    Revert "Ft 16.0 registration address search (#43)"
    This reverts commit f35382abc92c7c0fc894028fbe3813e9e03cdbb1.

--- src/Modules/Family/AddressComponent.js
+++ src/Modules/Family/AddressComponent.js
@@ -1,105 +1,59 @@
-import React, { Fragment, forwardRef } from "react";
-import StateDropdownComponent from "./StateDropdownComponent";
-import PlacesAutocomplete from "react-places-autocomplete";
+import React, { Fragment, forwardRef } from 'react';
+import StateDropdownComponent from './StateDropdownComponent';
 
-const AddressComponent = forwardRef(({ register, errors }, ref) => {
-  const [addressLine1, setAddressLine1] = React.useState("");
+const AddressComponent = forwardRef(({ register, errors }, ref) => (
+  <Fragment>
+    <h2>Where you live</h2>
+    <div className="form-group">
+      <label htmlFor="address_line_1">Street Address</label>
+      <input
+        type="text"
+        className="form-control"
+        name="address_line_1"
+        id="address_line_1"
+        ref={register({ required: true })}
+      />
+      {errors.address_line_1 && <span className="text-danger">This field is required</span>}
+    </div>
 
-  return (
-    <Fragment>
-      <h2>Where you live</h2>
-      <div className="form-group relative">
-        <label htmlFor="address_line_1">Street Address</label>
-        <PlacesAutocomplete value={addressLine1} onChange={setAddressLine1}>
-          {({
-            getInputProps,
-            suggestions,
-            getSuggestionItemProps,
-            loading,
-          }) => (
-            <>
-              <input
-                type="text"
-                className="form-control"
-                name="address_line_1"
-                id="address_line_1"
-                {...getInputProps()}
-                ref={register({ required: true })}
-                autoComplete="off"
-              />
-              {errors.address_line_1 && (
-                <span className="text-danger">This field is required</span>
-              )}
-              {/* No spinners are set here as of now. You can re-use the loader from EventContainer page; 
-                    though the size of the spinner is set as 10em,fixed in main.scss file. */}
-              {loading ? "Loading..." : null}
-              {suggestions.length > 0 && (
-                <div
-                  data-testid="suggestions"
-                  id="suggestions"
-                  name="suggestions"
-                  className="suggestions-container"
-                >
-                  {suggestions.map((suggestion) => {
-                    return (
-                      <div
-                        {...getSuggestionItemProps(suggestion)}
-                        key={suggestion.id}
-                      >
-                        {suggestion.description}
-                      </div>
-                    );
-                  })}
-                </div>
-              )}
-            </>
-          )}
-        </PlacesAutocomplete>
-      </div>
+    <div className="form-group">
+      <label htmlFor="address_line_2">apt/lot/suite</label>
+      <input
+        type="text"
+        className="form-control"
+        name="address_line_2"
+        id="address_line_2"
+        ref={register}
+      />
+    </div>
 
+    <div className="d-flex city-state-form">
       <div className="form-group">
-        <label htmlFor="address_line_2">apt/lot/suite</label>
+        <label htmlFor="city">City</label>
         <input
           type="text"
           className="form-control"
-          name="address_line_2"
-          id="address_line_2"
-          ref={register}
+          name="city"
+          id="city"
+          ref={register({ required: true })}
         />
+        {errors.city && <span className="text-danger">This field is required</span>}
       </div>
+      <StateDropdownComponent register={register} errors={errors} />
 
-      <div className="d-flex city-state-form">
-        <div className="form-group">
-          <label htmlFor="city">City</label>
-          <input
-            type="text"
-            className="form-control"
-            name="city"
-            id="city"
-            ref={register({ required: true })}
-          />
-          {errors.city && (
-            <span className="text-danger">This field is required</span>
-          )}
-        </div>
-        <StateDropdownComponent register={register} errors={errors} />
-
-        <div className="form-group ml-2">
-          <label htmlFor="zip_code">Zip Code</label>
-          <input
-            type="text"
-            className="form-control"
-            name="zip_code"
-            id="zip_code"
-            ref={register({ required: true })}
-          />
-          {errors.zip_code && (
-            <span className="text-danger">This field is required</span>
-          )}
-        </div>
+      <div className="form-group ml-2">
+        <label htmlFor="zip_code">Zip Code</label>
+        <input
+          type="text"
+          className="form-control"
+          name="zip_code"
+          id="zip_code"
+          ref={register({ required: true })}
+        />
+        {errors.zip_code && <span className="text-danger">This field is required</span>}
       </div>
-    </Fragment>
-  );
-});
+    </div>
+  </Fragment>
+));
 
 export default AddressComponent;
