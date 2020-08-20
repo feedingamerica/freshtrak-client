7c4e42716f2d925909ee08615b894a592dc0747c
Sean Lawrenz <seanlaw27@gmail.com>
2020-08-19 20:32:20 -0400

    initial state setup

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import eventReducer from './Events/eventSlice';
+import searchAddressReducer from './Search/searchSlice';
 
 export default configureStore({
   reducer: {
     event: eventReducer,
+    addressSearch: searchAddressReducer,
   }
 });
