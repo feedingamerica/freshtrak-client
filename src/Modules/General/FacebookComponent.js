f0413bac930c855e50c32f82bd173d3ea6594c4e
Mahesh Kurma <maheshruby405@gmail.com>
2020-09-01 21:06:46 -0400

    add react_facebook_login

---
+++ src/Modules/General/FacebookComponent.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import FacebookLogin from 'react-facebook-login';
+import axios from 'axios';
+import { API_URL } from '../../Utils/Urls';
+
+const FacebookComponent = ( ) => {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  // const [accessToken, setAccessToken] = React.useState("");
+
+  const componentClicked = () => console.log('clicked..');
+
+  const responseFacebook = async (response) => {
+    console.log(response);
+    setIsLoggedIn(true);
+    // setAccessToken(response.accessToken);
+
+    const { FB_URL_RESP } = API_URL;
+    try {
+      await axios({
+        method: 'post',
+        url: FB_URL_RESP,
+        data: JSON.stringify(response),
+        headers: { 'Content-Type': 'application/json' }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  let fbContent;
+
+  if(isLoggedIn){
+    fbContent = null
+  } else{
+    fbContent = (
+      <FacebookLogin
+        appId="2790354964530637"
+        size="small"
+        autoLoad={false}
+        onClick={componentClicked}
+        callback={responseFacebook}
+        icon="fa-facebook"
+      />
+    );
+  }
+
+  return (
+    <div>{fbContent}</div>
+  )
+};
+
+export default FacebookComponent;
--- src/Utils/Urls.js
+++ src/Utils/Urls.js
@@ -5,14 +5,15 @@
 export const BASE_URL = process.env.REACT_APP_PANTRY_FINDER_API;
 const REGISTRATION_URL = process.env.REACT_APP_REGISTRATION_API;
 export const API_URL = {
-  EVENTS_LIST: BASE_URL + 'api/agencies',
-  FOODBANK_LIST: BASE_URL + 'api/foodbanks',
-  EVENT_URL: BASE_URL + 'api/events',
-  EVENT_DATES_URL: BASE_URL + 'api/event_dates',
-  AGENCY_EVENTS: `${BASE_URL}api/agencies`,
-  GUEST_AUTH: `${REGISTRATION_URL}guest_authentications`,
-  GUEST_USER: `${REGISTRATION_URL}api/user`,
-  CREATE_RESERVATION: `${REGISTRATION_URL}api/reservations`,
+    EVENTS_LIST : BASE_URL + 'api/agencies',
+    FOODBANK_LIST : BASE_URL + 'api/foodbanks',
+    EVENT_URL : BASE_URL + 'api/events',
+    EVENT_DATES_URL : BASE_URL + 'api/event_dates',
+    AGENCY_EVENTS: `${BASE_URL}api/agencies`,
+    GUEST_AUTH: `${REGISTRATION_URL}guest_authentications`,
+    GUEST_USER: `${REGISTRATION_URL}api/user`,
+    CREATE_RESERVATION: `${REGISTRATION_URL}api/reservations`,
+    FB_URL_RESP: `${REGISTRATION_URL}auth_callbacks/facebook`,
 };
 
 export const RENDER_URL = {
