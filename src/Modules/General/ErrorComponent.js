bdc4a843e90b5dbb887e4bd1418b083f4b50a585
Mahesh Kurma <maheshruby405@gmail.com>
2020-09-15 11:36:51 -0400

    handle & show event date validation error messages

--- src/Modules/General/ErrorComponent.js
+++ src/Modules/General/ErrorComponent.js
@@ -3,17 +3,46 @@ import { RENDER_URL } from '../../Utils/Urls';
 import { Link } from 'react-router-dom';
 
 const ErrorComponent = ({error}) => {
+  const errors = error.event_date;
+
+  const viewErrors = () => {
+    if (!errors) {
+      return (
+        <div>
+          <h2 className="text-danger text-center mt-5 mb-5 mobile-mb">
+            Oops..! page not found
+          </h2>
+          <h4>
+            status: <b> {error.status} </b> <br/>
+            message: <b> {error.message} </b>
+          </h4>
+        </div>
+      )
+    } else if (errors) {
+    return (
+      <div>
+        <h2 className="text-danger mt-5 mb-5 mobile-mb">
+          validation errors found on event....!
+        </h2>
+        {errors &&
+          errors.map((error, index) => {
+            return <p key={`error-${error.status}`} className="">
+              status: <b> {error.code} </b> <br/> message: <b> {error.message} </b>
+              </p>
+          })
+        }
+      </div>
+    );
+    } else {
+      return null;
+    }
+  };
+
   return (
     <Fragment>
         <div className="mt-4">
           <section className="container pt-100 pb-100 ">
-            <h2 className="text-danger text-center mt-5 mb-5 mobile-mb">
-              Oops..! page not found
-            </h2>
-            <h4>
-              status: <b> {error.status} </b> <br/>
-              message: <b> {error.message} </b>
-            </h4>
+            { viewErrors() }
             <div className="button-wrap mt-4">
               <Link to={RENDER_URL.HOME_URL}>
                 <button
