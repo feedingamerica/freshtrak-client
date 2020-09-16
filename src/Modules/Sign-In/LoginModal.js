c8935f1c1a82dde3ba360aa9244db3d209ae44b4
sarath-yerra <sarathpy.02@gmail.com>
2020-09-16 10:06:04 -0400

    adding css and refactored login button component

--- src/Modules/Sign-In/LoginModal.js
+++ src/Modules/Sign-In/LoginModal.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
+import FacebookLoginComponent from '../General/FacebookLoginComponent';
 
 const LoginModalComponent = ({show, onLogin}) => {
   return (
-      <Modal show={show}>
+      <Modal show={show} className={'login-modal'}>
         <Modal.Header>
           <Modal.Title>
-            Guest Login
+            Login
           </Modal.Title>
         </Modal.Header>
         <Modal.Footer>
         <button type="submit" className="btn primary-button ml-1 flex-grow-1" onClick={onLogin}> Continue AS Guest </button>
+        <FacebookLoginComponent />
         </Modal.Footer>
       </Modal>
   );
