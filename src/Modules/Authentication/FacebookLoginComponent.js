418a51620e091899194b4e78d3170b4f003b07bb
mahesh kurma <maheshruby405@gmail.com>
2020-09-25 10:36:19 -0400

    add Authentication component- helps to refactor login as guest/facebook (#138)
    * add Authentication component- helps to refactor login as guest/facebook
    
    * update fb auth
    
    * remove isFbLoggedIn from localStorage after registration complete
    
    * pass FACEBOOK_APP_ID through env variable
    
    * authentication modal- added cose(X) button and update css
    
    Co-authored-by: David Pickering <davepickering1@gmail.com>

---
+++ src/Modules/Authentication/FacebookLoginComponent.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import FacebookLogin from 'react-facebook-login';
+
+const FacebookLoginComponent = ({onFbLogin} ) => {
+  const responseFacebook = async (response) => {
+    console.log(response);
+    onFbLogin(response);
+  }
+  const FACEBOOK_APP_ID = process.env.REACT_APP_FACEBOOK_APP_ID;
+
+  return (
+    <div style={{width: '100%'}}>
+      <FacebookLogin
+        appId={FACEBOOK_APP_ID}
+        size='small'
+        autoLoad={false}
+        fields="name,email,picture"
+        callback={responseFacebook}
+        icon="fa-facebook"
+        style={{width: '100%'}}
+      />
+    </div>
+  )
+};
+
+export default FacebookLoginComponent;
