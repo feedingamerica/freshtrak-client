4ac95e456c1fb82947ec74cb62829a30776c5029
mahesh kurma <maheshruby405@gmail.com>
2020-11-06 13:32:30 -0500

    update twilio sender phone no (#198)

--- src/Modules/Events/EventContainer.js
+++ src/Modules/Events/EventContainer.js
@@ -70,7 +70,6 @@ const EventContainer = props => {
   };
   const localUserToken = localStorage.getItem('userToken');
   localStorage.setItem('search_zip', `${zipCode}`);
-  
 
   return (
     <div>
