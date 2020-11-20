9ff0f3072de6181afd0299dec73feec0d4357a96
sarath-yerra <sarathpy.02@gmail.com>
2020-11-20 09:45:51 -0500

    adding text and remove button from the cards for registered cars

--- src/Modules/Home/HomeContainer.js
+++ src/Modules/Home/HomeContainer.js
@@ -13,16 +13,53 @@ import { setCurrentZip } from '../../Store/Search/searchSlice';
 import axios from 'axios';
 import '../../Assets/scss/main.scss';
 import EventListComponent from '../Events/EventListComponent';
-import { EventHandler } from '../../Utils/EventHandler';
+import { EventHandler, HomeEventFormat } from '../../Utils/EventHandler';
 import moment from 'moment';
 
 const HomeContainer = props => {
   const [agencyResponse, setAgencyResponse] = useState(false);
   const [agencyData, setAgencyData] = useState({});
+  const [reservedEvents, setReservedEvents] = useState([]); 
   const [zipCode, setZipCode] = useState(localStorage.getItem("search_zip"));
   let [searchDetails] = useState({});
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
+
+  useEffect( () =>{
+    getUsersReservations();
+  }, []);
+  const getUsersReservations = async () =>{
+    const userToken = localStorage.getItem('userToken');
+    const {CREATE_RESERVATION, EVENT_URL} = API_URL;
+    try {
+      const usersRegData = await axios.get(CREATE_RESERVATION, {
+        headers: { Authorization: `Bearer ${userToken}` }
+      });
+      // setUsersReservation(usersRegData.data);
+      getEventByDateId(usersRegData.data);
+      // setLoading();
+    } catch (e) {
+      console.log(e);
+    } 
+  }
+  const getEventByDateId = async (userRegData) =>{
+    const userRegEvents = []; 
+    const {EVENT_URL} = API_URL;
+    userRegData.map( (userReg) => {
+      userRegEvents.push(axios.get(`${EVENT_URL}?event_date_id=${userReg.event_date_id}`))
+    }
+    )
+    const regEvents = await axios.all(userRegEvents)
+    const events = [];
+    regEvents.forEach((event, index) => {
+      let filteredEvents;
+      if (event.data?.events[0]){
+         filteredEvents = HomeEventFormat(event.data.events[0],userRegData[index].event_date_id) 
+      }
+      filteredEvents && events.push(filteredEvents)
+    });
+    setReservedEvents(events);
+  }
 
   useEffect(() => {
     if (zipCode) {
@@ -99,11 +136,10 @@ const HomeContainer = props => {
       let agencyDataSorted = EventHandler(agencyData);
       agencyDataSorted = filterEvents(agencyDataSorted);
       // return <EventListComponent events={agencyDataSorted} zipCode={zipCode} showHeader= {false}/>;
-      return <EventListComponent targetUrl={RENDER_URL.REGISTRATION_EVENT_DETAILS_URL} events={agencyDataSorted} zipCode={zipCode} showHeader= {false}/>;
+      return <EventListComponent targetUrl={RENDER_URL.REGISTRATION_EVENT_DETAILS_URL} events={agencyDataSorted} zipCode={zipCode} showHeader= {false} reservedEvents = {reservedEvents}/>;
     }
     return <SpinnerComponent variant = "small" />;
   };
-
   return (
     <div>
       <section className="gray-bg">
@@ -125,7 +161,7 @@ const HomeContainer = props => {
           </div>
           <div className="foodbank-and-events">
             <LocalFoodBankComponent zipCode= {zipCode}/>
-            <UsersRegistrations/>
+            <UsersRegistrations reservedEvents = {reservedEvents}/>
             <EventNearByComponent EventList= {EventList}/> 
           </div>
           {/* {!loading && <EventList />} */}
