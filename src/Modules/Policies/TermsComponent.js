a0b28a074aba81911cd3f4a0cc97d21a7abc501a
sarath-yerra <sarathpy.02@gmail.com>
2021-01-15 13:48:38 -0500

    added styles to privacy policy and terms of use

--- src/Modules/Policies/TermsComponent.js
+++ src/Modules/Policies/TermsComponent.js
@@ -1,116 +1,120 @@
 import React, { Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
+import NavigationBtnComponent from "../General/NavigationBtnComponent";
 import '../../Assets/scss/main.scss';
 
 const TermsComponent = props => {
-  
-
   return (
     <Fragment>
-      <div>
+      <div className="container pt-100 pb-100 register-confirmation">
+      <div className="row d-none-xs">
+        <div className="col-md-12">
+          <NavigationBtnComponent />
+        </div>
+      </div>
       <h2 className="font-weight-bold mobile-text-left">
       FreshTrak Terms of Use
       </h2>
-      <p>Effective Date: January 01, 2021</p>
-      <p>
+      <p className="mobile-text-left caption-text">Effective Date: January 01, 2021</p>
+      <p className="mobile-text-left caption-text">
       These Terms of Use (these “Terms”) apply to our websites, microsites, and mobile versions of these websites (collectively, the “Website”) that expressly adopt and display or link to these Terms, as may be revised from time to time, and that are owned, operated, or controlled by FreshTrak, its affiliates and subsidiaries (collectively referred to as “FreshTrak,” “we,” “us,” or “our”). By accessing and using this Website, you (the “User”) acknowledge and agree to accept and be bound by these Terms. 
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       Your Use of this Website
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
       Your use of the Website confirms your acceptance of the Terms and your agreement to be bound by them. If you do not accept these Terms, do not use the Website. In the event of a violation of these Terms, we reserve the right to seek all remedies available by law and in equity. We may terminate your access or use of this Website at any time, for any reason, and without prior notice to you. 
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       Content
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
       All Website Content, including, without limitation, all text, graphics, audio, video, music, icons, images, hyperlinks, displays, products, HTML code and scripts (collectively, the “Website Content”), and the collection, arrangement and assembly of the Website Content, is the property of FreshTrak and its sponsors, distributors, or licensors, and is protected by copyright law and other U.S. and international laws and treaties.  The User is granted no right, title or interest in or to the Website Content other than the limited license expressly set forth in these Terms.  The User may not modify, publish, transmit, participate in the transfer or sale of, reproduce, create derivative works from, distribute, perform, display, incorporate into a website or in any way exploit the Website Content, or any portion thereof, without the express written permission of FreshTrak.
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       Trademarks
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
       All rights regarding any trademarks, trade names, service marks, logos and/or trade dress (collectively, “Marks”) contained in, or used in connection with, the Website are expressly reserved by FreshTrak and its licensors. The Marks, and all associated logos or images, are registered and/or common law trademarks, and are protected by U.S. and international laws and treaties. No license with respect to the use of the Marks is granted to the User under these Terms or by virtue of User’s use of the Website.  The Marks may not be copied, imitated or used, in whole or in part, without the prior written consent of FreshTrak or the applicable rights holder.
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       User Content
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
         The Website may allow the User to upload photographs, videos, or other content (“User Content”). The<br/>
         User shall be solely responsible for the User Content and the consequences of submitting and publishing the User Content via the Website. The User shall retain ownership of all User Content and hereby grants to FreshTrak a non-exclusive, royalty-free, transferable, worldwide right and license to use, reproduce, modify, adapt, publish, translate, transmit, distribute and display the User Content via the Website, and for our business purposes. The User also grants to the other users of the Website a non-exclusive license to access the User Content via the Website, and to use, reproduce, distribute, and display such User Content as permitted through the functionality thereof and subject to these Terms. The User grants FreshTrak the right to identify the User as the author of any such User Content by name, email address, or screen name, and the User acknowledges that FreshTrak has the right, but is not obligated, to use any such User Content and that FreshTrak may cease utilizing such User Content at any time for any reason. The User agrees not to submit, upload, or otherwise make available via the Website any content or materials (i) that are unlawful, threatening, abusive, harassing, defamatory, libelous, obscene, vulgar, profane, injurious to third parties, or which invade another person’s privacy, further the commission or concealment of a crime, or are otherwise objectionable; (ii) infringe the copyright or other intellectual property rights of a third party; or (iii) with respect to which the User does not have the rights necessary to transmit and publish such content or materials, or to grant FreshTrak the license to use such content or materials as described herein. The User acknowledges that FreshTrak has no obligation to monitor or screen User Content submitted to the Website, but that FreshTrak shall have the right (but not the obligation) to reject, remove or delete any User Content at its sole discretion.
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       License to Use 
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
       Subject to these Terms and any other agreement between you and us, we grant you a limited, personal, nonexclusive, nontransferable, revocable license to use the Website for your personal non-commercial purposes and in a manner enabled by us. This limited license allows you to download copies of materials on our Website for personal use only. This is a grant of license, not a transfer of title, and under this license you many not (i) modify or copy the materials, (ii) use the materials for any commercial purpose or for any public display, (iii) attempt to decompile or reverse engineer any software contained on our Website, (iv) remove any copyright or other proprietary notations from the materials, or (v) transfer the materials to another person. This license shall automatically be revoked if you violate any of these restrictions, and may be revoked by FreshTrak at any time.
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       Third Party Content
       </h4>
       <p>
       The User understands and agrees that FreshTrak does not control and is not responsible for any User Content made available on the Website by other users. The User’s use of, or reliance on, any information contained in such third party Content is at the User’s sole risk. Under no circumstances will FreshTrak be liable for any such third party Content or for any loss or damage resulting from the User’s use of, or reliance on, such third party Content.
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       Acceptable Use
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
       The User’s permitted use of the Website and Website content is limited to the standard functionality of the Website. In no event may the Website be used in a manner that (i) is unlawful, fraudulent, or deceptive, (ii) harasses, abuses, stalks, threatens, defames, or otherwise infringes or violates the rights of FreshTrak or any third party (including, without limitation, the rights of publicity or other proprietary rights), (iii) uses technology or other means not authorized by FreshTrak to access Website content, (iv) attempts to introduce viruses or any other computer code, files or programs that interrupt, destroy or limit the functionality of any computer software, hardware, or telecommunications equipment, (v) attempts to gain unauthorized access to FreshTrak’s systems or user accounts, (vi) encourages conduct that would constitute a criminal offense or that gives rise to civil liability, (vii) attempts to damage, disable or overburden FreshTrak’s servers or networks, or (viii) fails to comply with applicable third-party terms. The User must comply with all applicable third-party terms of use (e.g., wireless carrier terms of service) at all times while using the Website. FreshTrak reserves the right to, in its sole discretion, terminate the license granted to the User hereunder and/or bring legal action against the User if the User engages in, or FreshTrak reasonably believes the User has engaged in or is engaging in, use that violates these Terms. FreshTrak’s failure or delay in taking such actions does not constitute a waiver of its rights to enforce these Terms.
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       Eligibility; Jurisdiction
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
       The User represents and warrants that (i) the User is at least 13 years of age and is fully competent and able to enter into these Terms and to abide by the terms hereof; (ii) the User is not located in a country that is subject to a U.S. Government embargo or that has been designated by the U.S. Government as a “terrorist support” country, and (iii) the User is not included on any U.S. Government list of prohibited or restricted parties. The Website is not intended for children under the age of 13. FreshTrak provides the Website for use in the United States of America. FreshTrak does not represent the Website is available or appropriate for use in other jurisdictions. Any access to or use of the Website from other jurisdictions is at the User’s sole risk and the User is responsible for complying with all applicable local laws
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       Limitation of Liability
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
       You acknowledge and agree that, to the fullest extent provided by applicable law, FreshTrak will not be liable to you or to any other person under any circumstances or under any legal or equitable theory, whether in tort, contract, strict liability, or otherwise, for any indirect, special, incidental, or consequential losses or damages of any nature arising out of or in connection with the use of or inability to use the Website. To the fullest extent provided by applicable law, this limitation of liability applies to any damages or injury arising from any failure of performance, error, omission, interruption, deletion, defects, delay in operation or transmission, lost profits, loss of goodwill, loss of data, work stoppage, accuracy of results, computer failure or malfunction, computer viruses, file corruption, communication failure, network or system outage, theft, destruction, unauthorized access to, alteration of, loss of use of any record or data, and any other tangible or intangible loss. Subject to the foregoing, to the fullest extent provided by applicable law, FreshTrak will not be liable for any damages in connection with your use of this Website.
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       Disclaimer
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
       While we exercise reasonable efforts to ensure the accuracy of information on our Website and accessible through our services, you acknowledge and agree that your use of the Website or our services, and your reliance upon any data, information, or results accessed using the Website or services, is at your own risk. This Website and all content of the Website are provided to our Users “as is” and “as available” and without any warranties of any kind, whether express or implied, including but not limited to those of merchantability, fitness for a particular purpose, title, or noninfringement. You acknowledge by your use of the Website, that your use is at your sole risk. Some jurisdictions do not allow the disclaimer of implied warranties. In such jurisdictions, the foregoing disclaimer may not apply to you.
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       Indemnity
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
       Upon FreshTrak’s request, the User agrees to indemnify, defend and hold harmless FreshTrak and its employees, contractors, officers, directors, and licensors from and against all claims, suits, demands, damages, liabilities, loss, cost, and expense (including reasonable attorneys’ fees) that arise from: (i) the User’s use or misuse of the Website; (ii) the User’s violation of these Terms; or (iii) the User’s violation of any third party right, including without limitation any copyright, property, or privacy right. FreshTrak reserves the right to assume the exclusive defense and control of any matter otherwise subject to indemnification by the User, in which event the User will cooperate with FreshTrak in asserting any available defenses.
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       Social Media Policy
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
       Please note that any content you post to social medial platforms is subject to the data handling practices of each social media platform, as described in their respective terms of use and privacy policy, which you should read before posting content.
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       Governing Law, Arbitration, and Waiver of Class Action
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
       The laws of the State of Ohio (excluding any principles of conflicts of laws) govern your use of the Website, the services and these Terms. You agree that the parties shall settle any claim or dispute relating to these Terms by binding arbitration in Columbus, Ohio under the Commercial Arbitration Rules of the American Arbitration Association. To the fullest extent permitted by applicable law, no arbitration under these Terms shall be joined to an arbitration involving any other party subject to these Terms, whether through class arbitration proceedings or otherwise. Notwithstanding the dispute resolution requirements set forth above, for any injunctive relief relating to the access to, or use of, the Website including, without limitation, related services, you agree and hereby submit to the exclusive jurisdiction and venue of the appropriate state or federal courts located in Ohio.
       Whether in arbitration or court, you waive the right to prosecute or participate in class action, collective action, or other representative action. You may seek relief only on behalf of yourself and only to the extent necessary to remedy your individual claim. This class action waiver is a material and essential part of and cannot be severed from this provision.
       </p>
       <h4>
       Changes to Website
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
       At any time and without notice to the User, FreshTrak may change or modify the Website, or any features or functions thereof, or may suspend or discontinue the Website, or stop supporting the Website, or any aspect thereof. Any such termination, change, suspension or discontinuance shall be without liability to the User.
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       Changes to Terms
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
       FreshTrak reserves the right to change these Terms at any time, and in its sole discretion, by changing these Terms within the Website. The User’s continued use of the Website after any such changes to these Terms are made available through the Website will constitute the User’s acceptance of those changes. These Terms shall govern any updates to, or supplements or replacements for, the Website unless separate terms are provided in conjunction with such updates, supplements, or replacements – in which case such separate terms will apply.
       </p>
-      <h4>
+      <h4 className="mb-2 medium-title font-weight-bold">
       Contact Us
       </h4>
-      <p>
+      <p className="mobile-text-left caption-text">
       Please contact us with any questions you have. If you would like to contact us, please call [Insert Number] or send us an email at [Insert Email].
       </p>
     </div>
