bb6927c059b9d997514ba9e9b38f102420832034
maheshkurma <maheshruby405@gmail.com>
2021-01-15 12:07:13 -0500

    add distance dropdown to filter events on home page

--- src/Modules/Events/EventListContainer.js
+++ src/Modules/Events/EventListContainer.js
@@ -5,37 +5,35 @@ import { API_URL } from '../../Utils/Urls';
 import SpinnerComponent from '../General/SpinnerComponent';
 import axios from 'axios';
 
-const EventListContainer = ({ zipCode }) => {
+const EventListContainer = ({ zipCode , distance}) => {
   const [agencyResponse, setAgencyResponse] = useState(false);
   const [agencyData, setAgencyData] = useState({});
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    if (zipCode) {
-      getEvents(zipCode);
-    }
-  }, [zipCode]);
-
-  const getEvents = async zip => {
-    if (zip) {
-      setLoading(true);
-      try {
-        const resp = await axios.get(API_URL.EVENTS_LIST, {
-          params: { zip_code: zip },
-        });
-
-        const {
-          data: { agencies },
-        } = resp;
-        setAgencyData(agencies);
-        setAgencyResponse(true);
-        setLoading(false);
-      } catch (err) {
-        console.error(err);
-        setLoading(false);
+    const getEvents = async () => {
+      if (zipCode) {
+        setLoading(true);
+        try {
+          const resp = await axios.get(API_URL.EVENTS_LIST, {
+            params: { zip_code: zipCode, distance: distance },
+          });
+          const {
+            data: { agencies },
+          } = resp;
+          setAgencyData(agencies);
+          setAgencyResponse(true);
+          setLoading(false);
+        } catch (err) {
+          console.error(err);
+          setLoading(false);
+        }
       }
+    };
+    if (zipCode) {
+      getEvents();
     }
-  };
+  }, [zipCode, distance, ]);
 
   const EventList = () => {
     if (agencyResponse) {
