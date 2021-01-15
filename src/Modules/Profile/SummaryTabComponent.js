f7bd010f1ad5e26936ceab13edca3db51ce49ef9
Rajesh KP <rajesh.kp@inapp.com>
2021-01-15 15:37:57 +0530

    summary ui

--- src/Modules/Profile/SummaryTabComponent.js
+++ src/Modules/Profile/SummaryTabComponent.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import ResourceCategoryComponent from '../../Modules/Profile/ResourceCategoriesComponent';
+import BadgesComponent from '../../Modules/Profile/BadgesComponent';
+import YourReservationsComponent from '../../Modules/Profile/YourReservationsComponent';
 
 const SummaryTabComponent = () => {
   return(
-    <div className="container">
-      Summary
+    <div className="container pt-3 pb-3">
+      <ResourceCategoryComponent/>
+      <BadgesComponent/>
+      <YourReservationsComponent/>
     </div>
   )
 }
