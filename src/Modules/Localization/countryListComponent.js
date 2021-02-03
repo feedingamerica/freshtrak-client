76b2486dfa1ae40508d774a84c85cf8b94ce639b
sarath-yerra <sarathpy.02@gmail.com>
2021-02-02 19:48:42 -0500

    adding localization and adding languages

---
+++ src/Modules/Localization/countryListComponent.js
@@ -0,0 +1,20 @@
+// import React from 'react'
+// import { Dropdown } from 'semantic-ui-react'
+
+// const countryOptions = [
+//   { key: 'en', value: 'as', flag: 'as', text: 'American Samoa' },
+//   { key: 'spa', value: 'ad', flag: 'ad', text: 'Andorra' },
+//   { key: 'tur', value: 'ao', flag: 'ao', text: 'Angola' }
+// ]
+
+// const CountryListComponent = () => (
+//   <Dropdown
+//     placeholder='Select Country'
+//     fluid
+//     search
+//     selection
+//     options={countryOptions}
+//   />
+// )
+
+// export default CountryListComponent;
