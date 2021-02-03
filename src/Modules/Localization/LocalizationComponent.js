755ab834befa3d0bfc78f3553ff50a3c84079290
sarath-yerra <sarathpy.02@gmail.com>
2021-02-02 19:48:42 -0500

    adding localization and adding languages

---
+++ src/Modules/Localization/LocalizationComponent.js
@@ -0,0 +1,35 @@
+import LocalizedStrings from 'react-localization';
+
+let localization = new LocalizedStrings({
+ en:{
+   home_freshtrack:"FreshTrak is here to help!",
+   home_stay:"Stay Up to Date",
+   home_findfood:"Find Food",
+   home_comming_soon:"Coming soon... Make a FreshTrak account to stay up to date on local food access events.",
+   home_zip_details: "Enter your zip code and get connected to food access resources in your community."
+
+ },
+ spa: {
+   home_freshtrack:"FreshTrak está aquí para ayudar!",
+   home_stay:"Estar al día",
+   home_findfood:"Encontrar comida",
+   home_comming_soon:"Próximamente ... Cree una cuenta FreshTrak para mantenerse al día sobre los eventos locales de acceso a alimentos.",
+   home_zip_details:"Ingrese su código postal y conéctese a los recursos de acceso a alimentos en su comunidad."
+ },
+ tur: {
+  home_freshtrack:"FreshTrak yardım etmek için burada!",
+  home_stay:"Güncel kal",
+  home_findfood:"Yiyecek bul",
+  home_comming_soon:"Yakında ... Yerel yiyecek erişim etkinliklerinden haberdar olmak için bir FreshTrak hesabı oluşturun.",
+  home_zip_details:"Posta kodunuzu girin ve topluluğunuzdaki yiyecek erişim kaynaklarına bağlanın."
+},
+zho: {
+  home_freshtrack:"FreshTrak在這里為您提供幫助",
+  home_stay:"保持最新",
+  home_findfood:"找食物",
+  home_comming_soon:"即將推出...註冊一個FreshTrak帳戶，以隨時了解當地的食物獲取活動。",
+  home_zip_details:"輸入您的郵政編碼，並連接到您社區中的食物獲取資源。"
+}
+});
+
+export default localization;
