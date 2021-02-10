4b7dbc0fd192cef4432f0e5f107b6ff58fbd3cdd
sarath-yerra <sarathpy.02@gmail.com>
2021-02-09 23:36:18 -0500

    adding more chnages on lan

--- src/Modules/Localization/LocalizationComponent.js
+++ src/Modules/Localization/LocalizationComponent.js
@@ -9,7 +9,13 @@ let localization = new LocalizedStrings({
    home_zip_details: "Enter your zip code and get connected to food access resources in your community.",
    home_header_component:"We’re here to help! Input your zip code to find food access resources in your community.",
    home_dashboard: "Serve More Families",
-   home_dashboard_org: "Prepare your organization for the influx of demand for food by streamlining registration, planning service windows, and forecasting needs. Serve more families—both current and new—in your community."
+   home_dashboard_org: "Prepare your organization for the influx of demand for food by streamlining registration, planning service windows, and forecasting needs. Serve more families—both current and new—in your community.",
+   home_before_footer1: "Register neighbors ahead of time or,where possible, schedule a pick-up appointment, your organization can plan for any increase in demand.",
+   home_before_footer2: "You post food access “events” in your community—anything from daily pantry hours to pop-up food events—and neighbors enter their zip code to get connected to them.",
+   home_before_footer3: "Remove the bottleneck from distribution with online customer pre-registration. Serve more customers, safely!",
+   home_before_footer1_header: "Predict Need",
+   home_before_footer2_header: "Serve Food",
+   home_before_footer3_header:"Move Quickly"
 
  },
  spa: {
@@ -20,7 +26,13 @@ let localization = new LocalizedStrings({
    home_zip_details:"Ingrese su código postal y conéctese a los recursos de acceso a alimentos en su comunidad.",
    home_header_component:"¡Estamos aquí para ayudar! Ingrese su código postal para encontrar recursos de acceso a alimentos en su comunidad.",
    home_dashboard: "Sirva más familia",
-   home_dashboard_org: "Prepare su organización para la afluencia de demanda de alimentos mediante la optimización del registro, la planificación de las ventanas de servicio y la previsión de necesidades. Sirva a más familias, tanto nuevas como actuales, en su comunidad."
+   home_dashboard_org: "Prepare su organización para la afluencia de demanda de alimentos mediante la optimización del registro, la planificación de las ventanas de servicio y la previsión de necesidades. Sirva a más familias, tanto nuevas como actuales, en su comunidad.",
+   home_before_footer1: "Registre a los vecinos con anticipación o, cuando sea posible, programe una cita de recogida, su organización puede planificar cualquier aumento en la demanda.",
+   home_before_footer2: "Usted publica “eventos” de acceso a alimentos en su comunidad, desde el horario diario de la despensa hasta eventos emergentes de alimentos, y los vecinos ingresan su código postal para conectarse con ellos.",
+   home_before_footer3: "Elimine el cuello de botella de la distribución con el registro previo del cliente en línea. ¡Atienda a más clientes de forma segura!",
+   home_before_footer1_header: "Predecir la necesidadd",
+   home_before_footer2_header: "Servir comida",
+   home_before_footer3_header:"Muévete rápido"
  },
  som: {
   home_freshtrack:"FreshTrak waxay halkan u joogtaa inay ku caawiso!",
@@ -30,7 +42,13 @@ let localization = new LocalizedStrings({
   home_zip_details:"Gali nambarkaaga sifo aadna ugu xirnaato ilaha helitaanka cuntada ee bulshadaada.",
   home_header_component:"Waxaan halkaan u joognaa inaan ku caawinno! Gali nambarkaaga si aad uga hesho kheyraadka marin u helka cuntada bulshadaada.",
   home_dashboard: "U Adeer Qoys Dheeraad ah",
-  home_dashboard_org: "U diyaari ururkaaga qulqulka baahida cuntada adoo hagaajinaya diiwaangelinta, daaqadaha adeegga qorsheynta, iyo baahida saadaalinta. U adeegso qoysas badan - kuwa hadda iyo kuwa cusubba — ee bulshadaada dhexdeeda."
+  home_dashboard_org: "U diyaari ururkaaga qulqulka baahida cuntada adoo hagaajinaya diiwaangelinta, daaqadaha adeegga qorsheynta, iyo baahida saadaalinta. U adeegso qoysas badan - kuwa hadda iyo kuwa cusubba — ee bulshadaada dhexdeeda.",
+  home_before_footer1: "Diiwaangeli deriska ka hor waqtiga ama, halka ay suurtagal tahay, jadwal ballanta soo qaadista, ururkaagu wuxuu qorsheyn karaa koror kasta oo baahida ah.",
+  home_before_footer2: "Waxaad soo dhigtaa marin u helka cuntada “dhacdooyinka” ee bulshadaada dhexdeeda-wax kasta oo ka yimaada saacadaha maalinlaha ah ilaa munaasabadaha cuntada ee soo baxa - deriskuna waxay gashadaan nambarkooda si ay ula xiriiraan iyaga.",
+  home_before_footer3: "Ka saar culeyska ka jira qaybinta adoo diiwaangelinaya macaamiisha qadka tooska ah. U adeeg macaamiil badan, si badbaado leh!",
+  home_before_footer1_header: "Saadaal Baahi",
+  home_before_footer2_header: "Cunso Cunto",
+  home_before_footer3_header:"Si Deg Deg Ah U Dhaqaaq"
 },
 rus: {
   home_freshtrack:"FreshTrak здесь, чтобы помочь!",
@@ -40,7 +58,13 @@ rus: {
   home_zip_details:"Введите свой почтовый индекс и подключитесь к ресурсам по доступу к еде в вашем районе.",
   home_header_component:"Мы здесь, чтобы помочь! Введите свой почтовый индекс, чтобы найти ресурсы по доступу к еде в вашем районе.",
   home_dashboard: "Подавать больше семьи",
-  home_dashboard_org: "Подготовьте свою организацию к увеличению спроса на продукты питания путем оптимизации регистрации, планирования периодов обслуживания и прогнозирования потребностей. Обслуживайте больше семей - как нынешних, так и новых - в вашем районе."
+  home_dashboard_org: "Подготовьте свою организацию к увеличению спроса на продукты питания путем оптимизации регистрации, планирования периодов обслуживания и прогнозирования потребностей. Обслуживайте больше семей - как нынешних, так и новых - в вашем районе.",
+  home_before_footer1: "Зарегистрируйте соседей заранее или, если возможно, назначьте встречу с трансфером, ваша организация может спланировать любое увеличение спроса.",
+  home_before_footer2: "Вы публикуете в своем сообществе «события» доступа к еде - от ежедневных часов работы кладовой до всплывающих окон еды - и соседи вводят свой почтовый индекс, чтобы подключиться к ним.",
+  home_before_footer3: "Устраните узкое место в распространении с помощью предварительной онлайн-регистрации клиентов. Безопасно обслуживайте больше клиентов!",
+  home_before_footer1_header: "Прогнозировать потребность",
+  home_before_footer2_header: "Подавать еду",
+  home_before_footer3_header:"Быстро двигаться"
 },
  tur: {
   home_freshtrack:"FreshTrak yardım etmek için burada!",
@@ -50,7 +74,13 @@ rus: {
   home_zip_details:"Posta kodunuzu girin ve topluluğunuzdaki yiyecek erişim kaynaklarına bağlanın.",
   home_header_component:"Yardım etmek için buradayız! Topluluğunuzdaki yiyecek erişim kaynaklarını bulmak için posta kodunuzu girin.",
   home_dashboard: "Daha Fazla Aile Servis Et",
-  home_dashboard_org: "Kaydı düzene sokarak, hizmet pencerelerini planlayarak ve ihtiyaçları öngörerek kuruluşunuzu gıda talebi akışına hazırlayın. Topluluğunuzda hem mevcut hem de yeni olmak üzere daha fazla aileye hizmet edin."
+  home_dashboard_org: "Kaydı düzene sokarak, hizmet pencerelerini planlayarak ve ihtiyaçları öngörerek kuruluşunuzu gıda talebi akışına hazırlayın. Topluluğunuzda hem mevcut hem de yeni olmak üzere daha fazla aileye hizmet edin.",
+  home_before_footer1: "Komşularınızı önceden kaydedin veya mümkünse bir karşılama randevusu ayarlayın, kuruluşunuz talepte herhangi bir artış planlayabilir.",
+  home_before_footer2: "Topluluğunuzda günlük kiler saatlerinden pop-up yemek etkinliklerine kadar yiyecek erişimi etkinlikleri yayınlarsınız ve komşular bunlarla bağlantı kurmak için posta kodlarını girer.",
+  home_before_footer3: "Çevrimiçi müşteri ön kaydı ile darboğazı dağıtımdan kaldırın. Daha fazla müşteriye güvenle hizmet verin!",
+  home_before_footer1_header: "İhtiyacı Tahmin Et",
+  home_before_footer2_header: "Yemek servis etmek",
+  home_before_footer3_header:"Hızlı hareket et"
 },
 ara: {
   home_freshtrack:"FreshTrak هنا للمساعدة!",
@@ -60,7 +90,13 @@ ara: {
   home_zip_details:"أدخل الرمز البريدي الخاص بك واتصل بموارد الوصول إلى الطعام في مجتمعك.",
   home_header_component:"نحن هنا للمساعدة! أدخل الرمز البريدي الخاص بك للعثور على موارد الوصول إلى الطعام في مجتمعك.",
   home_dashboard: "خدمة المزيد من العائلة",
-  home_dashboard_org: "جهز مؤسستك لتدفق الطلب على الغذاء من خلال تبسيط التسجيل وتخطيط نوافذ الخدمة والتنبؤ بالاحتياجات. خدمة المزيد من العائلات - الحالية والجديدة - في مجتمعك."
+  home_dashboard_org: "جهز مؤسستك لتدفق الطلب على الغذاء من خلال تبسيط التسجيل وتخطيط نوافذ الخدمة والتنبؤ بالاحتياجات. خدمة المزيد من العائلات - الحالية والجديدة - في مجتمعك.",
+  home_before_footer1: "قم بتسجيل الجيران في وقت مبكر أو ، حيثما أمكن ، حدد موعدًا للاستلام ، يمكن لمؤسستك التخطيط لأي زيادة في الطلب.",
+  home_before_footer2: "تنشر أحداث الوصول إلى الطعام في مجتمعك — أي شيء بدءًا من ساعات تخزين الطعام اليومية إلى أحداث الطعام المنبثقة — ويقوم الجيران بإدخال الرمز البريدي الخاص بهم للتواصل معهم.",
+  home_before_footer3: "قم بإزالة عنق الزجاجة من التوزيع مع التسجيل المسبق للعملاء عبر الإنترنت. خدمة المزيد من العملاء بأمان!",
+  home_before_footer1_header: "توقع الحاجة",
+  home_before_footer2_header: "تقديم الطعام",
+  home_before_footer3_header:"تحرك بسرعة"
 },
 zho: {
   home_freshtrack:"FreshTrak在這里為您提供幫助",
@@ -70,7 +106,13 @@ zho: {
   home_zip_details:"輸入您的郵政編碼，並連接到您社區中的食物獲取資源。",
   home_header_component:"我们在这里为您提供帮助！输入您的邮政编码以查找社区中的食物获取资源。",
   home_dashboard: "服务更多家庭",
-  home_dashboard_org: "通过简化注册，计划服务窗口和预测需求，为组织准备好迎接食品需求的增长。在您的社区中为更多家庭提供服务（无论是现有的还是新的）。"
+  home_dashboard_org: "通过简化注册，计划服务窗口和预测需求，为组织准备好迎接食品需求的增长。在您的社区中为更多家庭提供服务（无论是现有的还是新的）。",
+  home_before_footer1: "提前注册邻居，或者在可能的情况下安排接送约会，您的组织可以计划任何需求的增长。",
+  home_before_footer2: "您可以在社区中发布食物获取事件（从每天的茶水时间到弹出式食物事件），并且邻居输入邮政编码以与他们建立联系。",
+  home_before_footer3: "通过在线客户预先注册，消除了分销的瓶颈。安全地服务更多的客户！",
+  home_before_footer1_header: "预测需求",
+  home_before_footer2_header: "提供食物",
+  home_before_footer3_header: "动作快点"
 },
 hin: {
   home_freshtrack:"FreshTrak मदद करने के लिए यहाँ है!",
@@ -80,7 +122,13 @@ hin: {
   home_zip_details:"अपना ज़िप कोड दर्ज करें और अपने समुदाय में भोजन पहुंच संसाधनों से जुड़े।",
   home_header_component:"हमलोग यहां सहायता करने के लिए हैं! अपने समुदाय में खाद्य पहुंच संसाधनों को खोजने के लिए अपना ज़िप कोड इनपुट करें।",
   home_dashboard: "अधिक परिवार की सेवा करें",
-  home_dashboard_org: "पंजीकरण, योजना खिड़की, और पूर्वानुमान आवश्यकताओं को सुव्यवस्थित करके भोजन की मांग की आमद के लिए अपने संगठन को तैयार करें। और अधिक परिवारों की सेवा करें - आपके समुदाय में वर्तमान और नए दोनों।"
+  home_dashboard_org: "पंजीकरण, योजना खिड़की, और पूर्वानुमान आवश्यकताओं को सुव्यवस्थित करके भोजन की मांग की आमद के लिए अपने संगठन को तैयार करें। और अधिक परिवारों की सेवा करें - आपके समुदाय में वर्तमान और नए दोनों।",
+  home_before_footer1: "समय से पहले पड़ोसियों को पंजीकृत करें या जहां संभव हो, पिक अप अपॉइंटमेंट शेड्यूल करें, आपका संगठन मांग में किसी भी वृद्धि के लिए योजना बना सकता है।",
+  home_before_footer2: "आप अपने समुदाय में भोजन पहुंच की घटनाओं को पोस्ट करते हैं - दैनिक पैंट्री घंटे से लेकर पॉप-अप फूड इवेंट तक कुछ भी - और पड़ोसी उनसे जुड़ने के लिए अपना ज़िप कोड दर्ज करते हैं।",
+  home_before_footer3: "ऑनलाइन ग्राहक पूर्व पंजीकरण के साथ वितरण से अड़चन को दूर करें। अधिक ग्राहकों की सेवा करें, सुरक्षित रूप से!",
+  home_before_footer1_header: "भविष्यवाणी की जरूरत है",
+  home_before_footer2_header: "भोजन परोसे",
+  home_before_footer3_header:"जल्दी चलो"
 },
 nep: {
   home_freshtrack:"फ्रेशट्र्याक मद्दत गर्न यहाँ छ!",
@@ -90,7 +138,13 @@ nep: {
   home_zip_details:"तपाईंको जिप कोड प्रविष्ट गर्नुहोस् र तपाईंको समुदायमा खाना पहुँच स्रोतहरूमा जडान हुनुहोस्।",
   home_header_component:"हामी यहाँ मद्दत गर्न आएका छौं! तपाईंको जिप कोड इनपुट गर्नुहोस् तपाईंको समुदायमा खाना पहुँच स्रोतहरू फेला पार्न।",
   home_dashboard: "अधिक Familie सेवा गर्नुहोस्",
-  home_dashboard_org: "पंजीकरण, योजना बनाउने सेवा विन्डोजहरू, र पूर्वानुमान आवश्यकताहरूलाई सुव्यवस्थित गरेर खानाको मागको आवकको लागि तपाईंको संगठन तैयार गर्नुहोस्। तपाईंको समुदायमा थप परिवारहरू - दुबै वर्तमान र नयाँ Ser सेवा गर्दछन्।"
+  home_dashboard_org: "पंजीकरण, योजना बनाउने सेवा विन्डोजहरू, र पूर्वानुमान आवश्यकताहरूलाई सुव्यवस्थित गरेर खानाको मागको आवकको लागि तपाईंको संगठन तैयार गर्नुहोस्। तपाईंको समुदायमा थप परिवारहरू - दुबै वर्तमान र नयाँ Ser सेवा गर्दछन्।",
+  home_before_footer1: "समय भन्दा पहिले छिमेकीहरू रेजिस्टर गर्नुहोस् वा जहाँ सम्भव छ, एक पिक-अप अपोइन्टमेन्ट अनुसूची गर्नुहोस्, तपाईंको संगठनले कुनै पनि मागको वृद्धिको लागि योजना गर्न सक्दछ।.",
+  home_before_footer2: "तपाईं आफ्नो समुदायमा खाना पहुँच “घटनाहरू” पोष्ट गर्नुहुन्छ - पप-अप खाना घटनाहरूमा दैनिक पेन्ट्री घण्टा देखि केहि पनि — र छिमेकीहरूले तिनीहरूसँग जडान हुनको लागि उनीहरूको जिप कोड प्रविष्ट गर्छन्।",
+  home_before_footer3: "अनलाइन ग्राहक पूर्व-दर्ता संग वितरण बाट अवरोध हटाउनुहोस्। अधिक ग्राहकहरूको सेवा गर्नुहोस्, सुरक्षित रूपमा!",
+  home_before_footer1_header: "भविष्यवाणी आवश्यकता",
+  home_before_footer2_header: "खाना परोस",
+  home_before_footer3_header:"द्रुत रूपमा सार्नुहोस्"
 }
 });
 
