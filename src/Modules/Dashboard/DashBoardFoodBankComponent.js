09cb3556dcc2529a839614dc445d5674e6e4ca0a
sarath-yerra <sarathpy.02@gmail.com>
2021-02-09 23:36:18 -0500

    adding more chnages on lan

--- src/Modules/Dashboard/DashBoardFoodBankComponent.js
+++ src/Modules/Dashboard/DashBoardFoodBankComponent.js
@@ -22,20 +22,20 @@ const DashBoardFoodBankComponent = () => (
     </p>
     <div className="row mt-5 text-center">
       <BoxComponent
-        title="Predict Need"
-        content="Register neighbors ahead of time or,where possible, schedule a pick-up appointment, your organization can plan for any increase in demand."
+        title={localization.home_before_footer1_header}
+        content={localization.home_before_footer1}
         imageUrl={PredictIcon}
         className=""
       />
       <BoxComponent
-        title="Serve Food"
-        content="You post food access “events” in your community—anything from daily pantry hours to pop-up food events—and neighbors enter their zip code to get connected to them."
+        title={localization.home_before_footer2_header}
+        content={localization.home_before_footer2}
         imageUrl={ServeFoodIcon}
         className=""
       />
       <BoxComponent
-        title="Move Quickly"
-        content="Remove the bottleneck from distribution with online customer pre-registration. Serve more customers, safely!"
+        title={localization.home_before_footer3_header}
+        content={localization.home_before_footer3}
         imageUrl={MoveQuickIcon}
         className=""
       />
