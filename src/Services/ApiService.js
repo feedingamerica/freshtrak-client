490d61866d63bb9a0c8c9f93d7852282ca612f59
maheshkurma <maheshruby405@gmail.com>
2021-03-11 06:36:58 -0500

    update email content

--- src/Services/ApiService.js
+++ src/Services/ApiService.js
@@ -4,15 +4,26 @@ import axios from "axios";
 import { API_URL } from '../Utils/Urls';
 import { CONFIRMATION_EMAIL } from '../Utils/Constants'
 
-export const sendRegistrationConfirmationEmail = async user => {
+const CLIENT_URL = process.env.REACT_APP_CLIENT_URL;
+
+const getReservationText = (location) => {
+  return location.state? `Your reservation time is at ${location.state.event_slot.start_time} - ${location.state.event_slot.end_time} on ${location.state.event_date}. `: "";
+}
+
+const getCodeURL = (identification_code) => {
+  return `Your QRCode for the Reservation ${CLIENT_URL}qrcode/${identification_code}`;
+}
+
+export const sendRegistrationConfirmationEmail = async (user, location) => {
   const { SEND_EMAIL } = API_URL;
   let identification_code =  user['identification_code']
   let from = CONFIRMATION_EMAIL['FROM']
   let subject = CONFIRMATION_EMAIL['SUBJECT']
   let to = user['email']
-  let content = CONFIRMATION_EMAIL['CONTENT'] + `${identification_code.toUpperCase()}.`
+  let content = `You have successfully registered for FreshTrak, ${getReservationText(location)} Your confirmation code is ${identification_code.toUpperCase()}.
+  ${getCodeURL(identification_code)}`
   try {
-    await axios.post(SEND_EMAIL, { from, subject, to, content});
+    await axios.post(SEND_EMAIL, { from, subject, to, content });
   } catch (e) {
     console.log(e);
   }
