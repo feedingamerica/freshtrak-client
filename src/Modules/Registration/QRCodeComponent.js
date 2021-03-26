c652dee4f006b4616b1564077feff96f15e9a2c7
sarath-yerra <sarathpy.02@gmail.com>
2021-03-26 14:06:11 -0400

    adding qr code event details on messgae

--- src/Modules/Registration/QRCodeComponent.js
+++ src/Modules/Registration/QRCodeComponent.js
@@ -7,7 +7,8 @@ const QRCodeComponent = () =>{
 
   return <div className="qrcode">
   <h2>
-    Your QR Code <br />
+  <center> <div>You have successfully registered. For more information please scan the QR Code</div></center>
+   <br />
     <center><QRCode value= "https://secure.pantrytrak.com/mobile/qr_code_processing.php?code={code.toUpperCase()}"/></center>
   </h2>
   <br />
