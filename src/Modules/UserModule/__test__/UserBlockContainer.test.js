bd3ab601a86dbe054cf1b621f8f7c6155f5b0757
basiltj07 <basiltj07@gmail.com>
2021-03-30 08:59:28 +0530

    FT-18.1-User-Module:Unit test added for userblock module

--- src/Modules/UserModule/__test__/UserBlockContainer.test.js
+++ src/Modules/UserModule/__test__/UserBlockContainer.test.js
@@ -71,12 +71,150 @@ test('Signup Api test',async ()=> {
     })
   })
   await signUpFunction().then(data => {
-    expect(signupResponse.CodeDeliveryDetails.Destination).toEqual('')
+    expect(signupResponse.CodeDeliveryDetails.Destination).toEqual(destination)
   })
 })
 
-/*
-code: "UsernameExistsException"
-message: "User already exists"
-name: "UsernameExistsException"
-*/
+test('User Already exist',async ()=> {   
+  const signUpFunction = jest.fn().mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+      reject({
+        code: "UsernameExistsException",
+        message: "User already exists",
+        name: "UsernameExistsException"
+      })
+    })
+  })
+  await signUpFunction().then(data => {
+    expect(signupResponse.CodeDeliveryDetails.Destination).toEqual('')
+  }).catch(err=>{
+    expect(err.code).toEqual('UsernameExistsException')
+  })
+})
+
+test('Signup Confirm success',async ()=> {
+  
+  const onConfirm = jest.fn().mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+      resolve({status:true})
+    })
+  })
+  await onConfirm().then(data => {
+    expect(data.status).toEqual(true)
+  })
+});
+
+test('Signup Confirm failed',async ()=> {  
+  const onConfirmFailed = jest.fn().mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+      reject({status:false})
+    })
+  })
+  await onConfirmFailed().then().catch(err=>{
+    expect(err.status).toEqual(false)
+  })
+});
+
+test('Verification code invalid',async ()=> {  
+  const onConfirmVerificationFailed = jest.fn().mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+      reject({
+        code: "CodeMismatchException",
+        message: "Invalid verification code provided, please try again.",
+      })
+    })
+  })
+  await onConfirmVerificationFailed().then().catch(err=>{
+    expect(err.code).toEqual('CodeMismatchException')
+  })
+})
+
+test('onResendConfirmCode API success',async ()=> {  
+  let verification_code ='89858';
+  const onResendConfirmCode = jest.fn().mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+      resolve({code:verification_code})
+    })
+  })
+  await onResendConfirmCode().then(data => {
+    expect(data.code).toEqual(verification_code)
+  })
+});
+
+test('onResendConfirmCode API Failed',async ()=> {  
+  const onResendConfirmCodeFailed = jest.fn().mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+      reject({
+        code: "CodeMismatchException",
+        message: "Invalid verification code provided, please try again.",
+      })
+    })
+  })
+  await onResendConfirmCodeFailed().then().catch(err=>{
+    expect(err.code).toEqual('CodeMismatchException')
+  })
+})
+
+
+test('Signin success',async ()=> {  
+  const onSignIn = jest.fn().mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+      resolve({status:true})
+    })
+  })
+  await onSignIn().then(res=>{
+    expect(res.status).toEqual(true)
+  })
+});
+
+test('Signin Failed',async ()=> {  
+  const onSignInFailed = jest.fn().mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+      reject({
+        code: "NotAuthorizedException",
+        message: "Incorrect username or password.",
+      })
+    })
+  })
+  await onSignInFailed().then().catch(err=>{
+    expect(err.code).toEqual('NotAuthorizedException')
+  })
+});
+
+test('Signin with MFA',async ()=> {  
+  const onSignInWithMFA = jest.fn().mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+      resolve({
+        challengeName: "SMS_MFA",
+      })
+    })
+  })
+  await onSignInWithMFA().then(res => {
+    expect(res.challengeName).toEqual('SMS_MFA')
+  });
+});
+
+test('SignIn Confirm MFA success',async ()=> {  
+  const onConfirmPhone = jest.fn().mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+      resolve({status:true})
+    })
+  })
+  await onConfirmPhone().then(data => {
+    expect(data.status).toEqual(true)
+  })
+});
+
+test('SignIn Confirm MFA failed due to invalid code',async ()=> {  
+  const onSignInMFAFailed = jest.fn().mockImplementation(() => {
+    return new Promise((resolve, reject) => {
+      reject({
+        code: "CodeMismatchException",
+        message: "Invalid verification code provided, please try again.",
+      })
+    })
+  })
+  await onSignInMFAFailed().then().catch(err=>{
+    expect(err.code).toEqual('CodeMismatchException')
+  })
+});
