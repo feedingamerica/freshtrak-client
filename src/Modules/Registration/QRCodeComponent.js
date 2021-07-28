2ff7e0963696ce10b654abfdb9d69e703164572c
maheshkurma <maheshruby405@gmail.com>
2021-07-28 17:46:42 -0400

    fix qr_code url

--- src/Modules/Registration/QRCodeComponent.js
+++ src/Modules/Registration/QRCodeComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import QRCode from 'qrcode.react';
+import {useParams} from 'react-router-dom';
 
 const QRCodeComponent = () =>{
+  const {code}  = useParams();
+
   return <div className="qrcode">
   <h2>
-  <center> <div>You have successfully registered. For more information please scan the QR Code</div></center>
-   <br />
-    <center><QRCode value= "https://secure.pantrytrak.com/mobile/qr_code_processing.php?code={code.toUpperCase()}"/></center>
+    Your QR Code <br />
+    <center><QRCode value= {`https://secure.pantrytrak.com/mobile/qr_code_processing.php?code=${code.toUpperCase()}`}/></center>
   </h2>
   <br />
 </div>
